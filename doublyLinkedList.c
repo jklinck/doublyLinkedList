4dd43fb6010a57368a1b8c57a9a5427d289560d1
Joseph Klinck <josephklinck@Josephs-MacBook-Pro.local>
2024-05-24 15:24:22 -0700

    added comments to removeNode function to make it easier to follow

--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -39,6 +39,7 @@ void print(struct ll* head){
     struct ll* temp = head;
     printf("[");
     while(temp != NULL){
+        // prints closing bracket after tail
         if(temp->next == NULL){
             printf("%d]\n", temp->data);
             return;
@@ -58,17 +59,20 @@ void clear(struct ll** head){
         free(current);
         current = temp;
     }
+    // set head to NULL to prevent memory leak
     *head = NULL;
 }
 
 // remove a node
 void removeNode(struct ll** head, int nodeData){
+    // check if list is empty
     if(*head == NULL){
         printf("The list is empty, so you cannot remove that node.\n");
         return;
     }
     struct ll* current = *head;
 
+    // removes head, head->next becomes new head
     if((*head)->data == nodeData){
         (*head)->next->prev = NULL;
         *head = (*head)->next;
@@ -76,20 +80,24 @@ void removeNode(struct ll** head, int nodeData){
         return;
     }
 
+    // iterates through list looking for nodeData
     while(current->data != nodeData){    
         current = current->next;
+        // check for nodeData not being present in list
         if(current == NULL){
             printf("That node does not exist in this list.\n");
             return;
         }
     }
 
+    // removes tail
     if(current->next == NULL){
         current->prev->next = NULL;
         free(current);
         return;
     }
 
+    // removes nodeData when nodeData is not the head or tail
     current->next->prev = current->prev;
     current->prev->next = current->next;
     free(current);
